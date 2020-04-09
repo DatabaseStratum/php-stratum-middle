fbc924d93a82907438dc5ed86e78562e5f9cf19d
Paul Water <p.r.water@setbased.nl>
2020-04-10 00:21:43 +0200

    Fixed typo.

--- src/Exception/DataLayerException.php
+++ src/Exception/DataLayerException.php
@@ -6,7 +6,7 @@ namespace SetBased\Stratum\Middle\Exception;
 use SetBased\Exception\NamedException;
 
 /**
- * Exception for situations where the execution of s SQL query has failed.
+ * Exception for situations where the execution of SQL query has failed.
  */
 interface DataLayerException extends NamedException
 {
